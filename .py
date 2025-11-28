47525354162e431d9ac1d7bd6b3e166f2a2b666e
IsaqueCR4 <isaquecostarodrigues3@gmail.com>
2025-11-28 17:36:38 -0300

    Create project description in .py file
    Added project description and repository information.

---
+++ .py
@@ -0,0 +1,3 @@
+PROJETO-AVALIACÃO_REGIMENTAL-A1 ##Projeto de fundamentos em pytons — Avaliação A1
+
+Repositório destinado à Avaliação A1 da disciplina Técnicas de Desenvolvimento de Algoritmos, com foco em Estruturas Condicionais, Estruturas de Repetição, Listas e Dicionários, desenvolvido por meio de atividades práticas durante o curso.
